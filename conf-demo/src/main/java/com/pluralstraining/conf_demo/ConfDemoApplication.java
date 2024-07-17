51fdd1a335afd82c544ea5c30b68567f80b8027c
Ntoampe <Legopo.Ntoampe@standardbank.co.za>
2024-07-17 08:58:15 +0200

    Project added

---
+++ conf-demo/src/main/java/com/pluralstraining/conf_demo/ConfDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pluralstraining.conf_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfDemoApplication.class, args);
+	}
+
+}
---
+++ conf-demo/src/test/java/com/pluralstraining/conf_demo/ConfDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralstraining.conf_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
