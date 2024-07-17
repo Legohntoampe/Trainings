54ef6ce58bc06a9cd0e61863297e14d49e0131b4
Ntoampe <Legopo.Ntoampe@standardbank.co.za>
2024-07-17 12:09:06 +0200

    look for homework parts in the SessionController this will also be done in the SpeakersController

--- conf-demo/src/main/java/com/pluralstraining/conf_demo/controllers/SessionsController.java
+++ conf-demo/src/main/java/com/pluralstraining/conf_demo/controllers/SessionsController.java
@@ -3,6 +3,7 @@ package com.pluralstraining.conf_demo.controllers;
 
 import com.pluralstraining.conf_demo.models.Session;
 import com.pluralstraining.conf_demo.repositories.SessionRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -25,11 +26,28 @@ public class SessionsController {
         return sessionRepository.getOne(id);
         //GETTING SPECIFIC SESSION BY SESSION_ID
     }
-    // postong to the table?
+
     @PostMapping
-   // @ResponseStatus(HttpStatus.CREATED)//
+   // @ResponseStatus(HttpStatus.CREATED)//specifies the response we want
     public Session create(@RequestBody final Session session){
+        //this method will create a new session if uyou pass a session info to the api endpoint
         return sessionRepository.saveAndFlush(session);
+    }
+    @RequestMapping(value="id",method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id){
+        //will need to check for children records before deleting
+        //HOMEWORK:find and  put in loggic that allows the deleting of children records for session records in our db
+        sessionRepository.deleteById(id);
+    }
+    @RequestMapping(value = "id",method = RequestMethod.PUT)
+    public Session update(@PathVariable Long id,@RequestBody Session session){
+        // NB: for updating we have 2 verbs we can use ie. PUT & PATCH,
+        //PUT replaces all the attributes to be passed to the record
+        //PATCH allows for the update to a portion of the attributes in the record
+        //HOMEWORK: add validation that all attributes are passed in, otherwise return a 400 payload
+        Session existingSession = sessionRepository.getOne(id);
+        BeanUtils.copyProperties(session, existingSession, "session_id");
+        return sessionRepository.saveAndFlush(existingSession);
     }
 
 }
--- conf-demo/src/main/java/com/pluralstraining/conf_demo/controllers/SpeakersController.java
+++ conf-demo/src/main/java/com/pluralstraining/conf_demo/controllers/SpeakersController.java
@@ -3,11 +3,9 @@ package com.pluralstraining.conf_demo.controllers;
 import com.pluralstraining.conf_demo.models.Session;
 import com.pluralstraining.conf_demo.models.Speaker;
 import com.pluralstraining.conf_demo.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
@@ -23,5 +21,20 @@ public class SpeakersController {
     @RequestMapping("{id}")
     public Speaker get(@PathVariable Long id){
         return speakerRepository.getOne(id);
+    }
+    @PostMapping
+    public Speaker create(@RequestBody final Speaker speaker){
+        return speakerRepository.saveAndFlush(speaker);
+    }
+
+    @RequestMapping(value ="id", method =RequestMethod.DELETE)
+    public void delete(@PathVariable Long id){
+        speakerRepository.deleteById(id);
+    }
+    @RequestMapping(value = "id", method = RequestMethod.PUT)
+    public Speaker update(@PathVariable Long id,@RequestBody Speaker speaker){
+        Speaker speakerExisting = speakerRepository.getOne(id);
+        BeanUtils.copyProperties(speaker,speakerExisting,"speaker_id");
+        return speakerRepository.saveAndFlush(speakerExisting);
     }
 }
